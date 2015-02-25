b9cdc83d0dc41c1a8061d83b312e57a82ccbdb2f
Sajoku <sajoku@me.com>
2015-02-25 20:41:13 +0100

    Split the given amount (income) into 4 amounts
    * This enables to do the tax calculations on the correct amounts.
    Return the amounts which are based on
    (http://www.homefinance.nl/hypotheek/informatie/maandlasten-hypotheek-be
    rekenen/berekenen-belastbaar-inkomen.asp).

--- lib/taxable.rb
+++ lib/taxable.rb
@@ -1,5 +1,32 @@
 require "taxable/version"
 
 module Taxable
-  # Your code goes here...
+
+  def self.split_income(income)
+    case
+    when income <= 1982200
+      first_amount = 1982200
+      second_amount = 0
+      third_amount = 0
+      fourth_amount = 0
+    when income.between?(1982300, 3358900)
+      first_amount = 1982200
+      second_amount = income - 1982200
+      third_amount = 0
+      fourth_amount = 0
+    when income.between?(3359000, 5758500)
+      first_amount = 1982200
+      second_amount = 1376700
+      third_amount =  income - first_amount - second_amount
+      fourth_amount = 0
+    when income >= 5758600
+      first_amount = 1982200
+      second_amount = 1376700
+      third_amount = 2399600
+      fourth_amount =  income - first_amount - second_amount - third_amount
+    end
+
+    [first_amount, second_amount, third_amount, fourth_amount]
+  end
+
 end
--- spec/taxable_spec.rb
+++ spec/taxable_spec.rb
@@ -5,7 +5,39 @@ describe Taxable do
     expect(Taxable::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "splitting the income based on the known brackets" do
+    it "returns the first amount if under or equal to 1982200" do
+      amount = Taxable.split_income(1982200)
+      expect(amount).to eq([1982200, 0, 0, 0])
+    end
+
+    it "returns the first and second amount if above 1982300" do
+      amount = Taxable.split_income(2200000)
+      expect(amount).to eq([1982200, 217800, 0, 0])
+    end
+
+    it "returns the first, second and third amount if between € 33.590 and € 57.585" do
+      amount = Taxable.split_income(3800000)
+      expect(amount).to eq([1982200, 1376700, 441100, 0])
+    end
+
+
+    it "returns the first, second, third and fourth amount if above 5785600" do
+      amount = Taxable.split_income(7700000)
+      expect(amount).to eq([1982200, 1376700, 2399600, 1941500])
+    end
+
   end
+
+  it "calculates the income based on price" do
+    pending "first implement the split income tests"
+    net_income = Taxable.calculate_net_income_for(19822)
+    expect(net_income).to eq(12587)
+  end
+
+  it "calculate with bracket one and two for a net income of 22.000" do
+    net_income = Taxable.calculate_net_income_for(22000)
+    expect(net_income).to eq(13851)
+  end
+
 end
